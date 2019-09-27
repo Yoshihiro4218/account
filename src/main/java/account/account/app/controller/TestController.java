7727f1ba34f7234e9c768754f13087a677c15ee1
kadono <y.kadono4218@gmail.com>
2019-09-27 16:32:11 +0900

    user名取得

--- src/main/java/account/account/app/controller/TestController.java
+++ src/main/java/account/account/app/controller/TestController.java
@@ -1,6 +1,7 @@
 package account.account.app.controller;
 
 import lombok.extern.slf4j.*;
+import org.springframework.security.core.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,13 @@ import javax.servlet.http.*;
 public class TestController {
 
     @GetMapping("/test")
-    public String test(Model model, HttpServletRequest httpServletRequest) {
+    public String test(Model model, HttpServletRequest httpServletRequest, Authentication authentication) {
         log.info("user={}", httpServletRequest.getRemoteUser());
+
+        log.info("auth={}", authentication.getName());
+
+        log.info("authPrincipal={}", authentication.getPrincipal());
+
         model.addAttribute("user", httpServletRequest.getRemoteUser());
         return "pages/index";
     }
