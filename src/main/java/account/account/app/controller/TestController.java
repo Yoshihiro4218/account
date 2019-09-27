09cc896fa0ac33e5ef560b26d2822511a80977b3
kadono <y.kadono4218@gmail.com>
2019-09-27 15:40:56 +0900

    getRemoteUserでuser名（email）を取得するサンプル

--- src/main/java/account/account/app/controller/TestController.java
+++ src/main/java/account/account/app/controller/TestController.java
@@ -1,13 +1,20 @@
 package account.account.app.controller;
 
+import lombok.extern.slf4j.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.*;
+
 @Controller
+@Slf4j
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
+    public String test(Model model, HttpServletRequest httpServletRequest) {
+        log.info("user={}", httpServletRequest.getRemoteUser());
+        model.addAttribute("user", httpServletRequest.getRemoteUser());
         return "pages/index";
     }
 }
