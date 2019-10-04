ce75b2782e8287afbd23260b122ece0bd0ef5a34
kadono <y.kadono4218@gmail.com>
2019-10-04 19:01:56 +0900

    LoginControllerTest.java追加するも落ちるためコメントアウト

---
+++ src/test/java/account/account/controller/LoginControllerTest.java
@@ -0,0 +1,40 @@
+//package account.account.controller;
+//
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.eq;
+//import static org.mockito.Mockito.never;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verifyNoMoreInteractions;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+//
+//import account.account.app.controller.*;
+//import org.junit.*;
+//import org.junit.runner.*;
+//import org.springframework.beans.factory.annotation.*;
+//import org.springframework.boot.test.autoconfigure.web.servlet.*;
+//import org.springframework.boot.test.mock.mockito.*;
+//import org.springframework.test.context.junit4.*;
+//import org.springframework.test.web.servlet.*;
+//
+//
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(LoginController.class)
+//public class LoginControllerTest {
+//
+//    @Autowired
+//    MockMvc mockMvc;
+//    @MockBean
+//
+//
+//    @Test
+//    public void loginForm() throws Exception {
+//        mockMvc.perform(get("/login"))
+//                .andExpect(status().isOk())
+//                .andExpect(view().name("pages/loginForm"));
+//    }
+//}
