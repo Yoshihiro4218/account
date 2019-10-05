07ed2d9cdb40670cf6705dd22b91c10997822950
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-10-05 14:26:12 +0900

    cosmetic

--- src/test/java/account/account/controller/AccountControllerTest.java
+++ src/test/java/account/account/controller/AccountControllerTest.java
@@ -1,14 +1,12 @@
 package account.account.controller;
 
 import account.account.app.controller.AccountController;
-import account.account.app.controller.LoginController;
 import account.account.domain.entity.Account;
 import account.account.domain.entity.AccountForm;
 import account.account.domain.service.AccountService;
 import account.account.domain.service.JpaUserDetailsServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,7 +14,6 @@ import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequ
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
