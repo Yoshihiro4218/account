63f2e271ee90aacad83fe9071be4c2f0511fcb14
Yoshihiro4218 <nana.mizuki77@icloud.com>
2019-10-05 14:25:04 +0900

    AccountFormのvalidation追加

--- src/main/java/account/account/domain/entity/AccountForm.java
+++ src/main/java/account/account/domain/entity/AccountForm.java
@@ -2,10 +2,13 @@ package account.account.domain.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @Data
 public class AccountForm implements Serializable {
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 }
